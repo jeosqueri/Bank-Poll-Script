a58e384f11065632e9915b5ce5e52119f1e266af
jeosqueri <juliasqueri303@gmail.com>
2020-09-29 16:56:25 -0600

    Figured out how to get differences for profit/loss column and store those in new list. Continued modifying/working with code for bank solution

--- PyBank/mainbank.py
+++ PyBank/mainbank.py
@@ -3,56 +3,55 @@ import os
 import csv
 
 #Set path for CSV file
-csvpath = os.path.join('..','Resources','budget_data_main.csv')
+#Currently not working when I don't set the directory like this... fix this
+csvpath = os.path.join('/Users','juliasqueri','Desktop','python_challenge','PyBank','Resources','budget_data_main.csv')
 
 months = []
+profit_loss = []
 
 #Define functions
-#def financial_anal(budget_data):
-    #date = str(budget_data[0])
-    #profit_loss = int(budget_data[1])
+#Calcuate average of changes in Profit/Loss- AVERAGE FORMULA RIGHT, 
+# but needs to be calculating the average of the net profit/loss, so need to calculate that first
+def average(profit_loss):
+    length = len(profit_loss)
+    total = 0
+    for number in profit_loss:
+        total += number
+    return total / length
 
     #Count total number of months
-    #total_months = count(date)
+    
 
     #Calculate net total amount of Profit/Loss
-    #net_total = 
+    
     #Calcuate average of changes in Profit/Loss
 
-
 #Open CSV file
-with open(csvpath) as csvfile:
+with open(csvpath, mode = 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #Skip header using next function
     next(csvreader)
     for row in csvreader:
-        #Define date and profit/loss inside loop
-        #date = row[0]
-        #proft_loss = row[1]
-
+        
         #Add months to list
         months.append(row[0])
 
+        #Add profit/loss to list
+        profit_loss.append(int(row[1]))
     
-
-        #counts = []
-
-        #for i in months:
-            #counts[int(months)] = (counts[int(months)] + 1)
-            #print(counts)
+        #Generate successive differences for profit list
+        diff = [profit_loss[i + 1]- profit_loss[i] for i in range(len(profit_loss)-1)]
+        
+        #Now get average of diff  
 
     
-months.count(row[0])
+#months.count(row[0])
 
-print(months.count(date))
+#print(months.count)
 
+#print("Average is" + str(average(profit_loss)))
 
 #print(f"Total months: {total_months}")
 #print(f"Total Profit: {total_profit}")
 #print(f"Average change: {av_change}")
-#print(f"Maximum: {max(total_profit)}")
-#print(f"Minimum: {min(total_profit)}")
-
-
-
-    
+#print(f"Maximum:{max(total_profit)}")
