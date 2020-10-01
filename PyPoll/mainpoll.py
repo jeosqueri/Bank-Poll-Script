5d76d7e931416e229f1dc6de460060f30fa35c8e
jeosqueri <juliasqueri303@gmail.com>
2020-10-01 16:57:04 -0600

    Worked on formatting print statements for candidates and votes they recieved

--- PyPoll/mainpoll.py
+++ PyPoll/mainpoll.py
@@ -23,6 +23,13 @@ candidate_dic = {}
 total_votes = []
 candidates = []
 
+def vote_percentage(dictionary, s):
+    sum = 0
+    for key in dictionary:
+        sum += dictionary[key]
+        percentage = '{0:.2f}'.format(float(dictionary[s])/float(sum)*100)
+    return percentage
+
 #Open CSV file
 with open(csvpath, mode = 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -42,11 +49,25 @@ with open(csvpath, mode = 'r') as csvfile:
 
 print(candidate_dic)
 
+#vote_percent = []
+#for key in candidate_dic:
+    #vote_percent = (round(value/total_votes*100,3))
+#for key, value in candidate_dic.items():
+    #print(" ".join(key) + " {0} %".format(str(round(value/total_votes*100, 2))))
+#print(vote_percent)
 #Print total number of votes cast
 print("Total Votes: " + str(len(total_votes)))
 
+print("Khan: " + str(candidate_dic["Khan"]) + " " + str(vote_percentage(candidate_dic, "Khan")))
+print("Khan: " + str(vote_percentage(candidate_dic, "Khan")) + "%" + " " + "(" + str(candidate_dic["Khan"]) + ")")
+print("Correy: " + str(vote_percentage(candidate_dic, "Correy")) + "%" + " " + "(" + str(candidate_dic["Correy"]) + ")")
+print("Li: " + str(vote_percentage(candidate_dic, "Li")) + "%" + " " + "(" + str(candidate_dic["Li"]) + ")")
+print("O'Tooley: " + str(vote_percentage(candidate_dic, "O'Tooley")) + "%" + " " + "(" + str(candidate_dic["O'Tooley"]) + ")")
+#print(f"Khan: {vote_percentage(candidate_dic, "Khan")}% ( {candidate_dic["Khan"]} )")
 #Call unique function to print list of candidates
 #print(unique(candidates))
+
+
 
 
 
