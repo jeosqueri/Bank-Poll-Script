ea828a7b0e45c8c90d416a2646b1280b82957923
jeosqueri <juliasqueri303@gmail.com>
2020-10-01 13:26:27 -0600

    Figured out how to create candidate dictionary to store unique names and vote total per candidate for PyPoll assignment. Created dictionary and got candidate total numbers and total votes

--- PyPoll/mainpoll.py
+++ PyPoll/mainpoll.py
@@ -6,19 +6,22 @@ import csv
 #Currently not working when I don't set the directory like this... fix this
 csvpath = os.path.join('/Users','juliasqueri','Desktop','python_challenge','PyPoll','Resources','election_data.csv')
 
-total_votes = []
-candidates = []
+#total_votes = []
+#candidates = []
 
 #Create unique name function for generating list of candidate names
-def unique(candidates):
-    unique_list = []
+#def unique(candidates):
+    #unique_list = []
+    
+    #unique_candidates = set(candidates)
 
-    unique_candidates = set(candidates)
+    #for candidates in unique_candidates:
+        #unique_list.append(candidates)
 
-    for candidates in unique_candidates:
-        unique_list.append(candidates)
-
-    return unique_list
+    #return unique_list
+candidate_dic = {}
+total_votes = []
+candidates = []
 
 #Open CSV file
 with open(csvpath, mode = 'r') as csvfile:
@@ -26,22 +29,24 @@ with open(csvpath, mode = 'r') as csvfile:
     #Skip header using next function
     next(csvreader)
     for row in csvreader:
-
-        #Add each individual vote to total votes list for total of votes cast
+        
         total_votes.append(row[0])
-
-        candidates.append(row[2])
-
-        #Count votes per candidate by creating dictionary
         
-        
+        candidate = (row[2])
 
+        if candidate in candidate_dic:
+            candidate_dic[candidate]+=1
+        else:
+            candidate_dic[candidate]=1
+
+
+print(candidate_dic)
 
 #Print total number of votes cast
 print("Total Votes: " + str(len(total_votes)))
 
 #Call unique function to print list of candidates
-print(unique(candidates))
+#print(unique(candidates))
 
 
 
